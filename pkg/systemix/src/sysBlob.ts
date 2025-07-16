108929bff62a6e4381bf521dcc2a35c33ffbd681
charlyoleg <charles.braquet@gmx.fr>
2025-07-16 21:36:00 +0200

    fix singleton

--- pkg/systemix/src/sysBlob.ts
+++ pkg/systemix/src/sysBlob.ts
@@ -5,32 +5,38 @@ interface tBlob {
 	fBlob: string;
 }
 
-class sysBlob {
-	fBlob: tBlob[];
+class SysBlob {
+	static instance: SysBlob;
+	static fNB: tBlob[];
 	constructor() {
-		this.fBlob = [];
-		console.log('dbg012: instanciate singleton sBlob');
+		console.log('dbg012: instanciate singleton sBlob start');
+		if (SysBlob.instance) {
+			return SysBlob.instance;
+		}
+		SysBlob.fNB = [];
+		SysBlob.instance = this;
+		console.log('dbg018: instanciate singleton sBlob end');
 	}
 	getNames(): string[] {
-		const rNames = this.fBlob.map((iblob) => iblob.fName);
+		const rNames = SysBlob.fNB.map((iblob) => iblob.fName);
 		return rNames;
 	}
 	listNames() {
 		const fNames = this.getNames();
-		console.log('sysBlob list:');
+		console.log('SysBlob list:');
 		for (const [idx, one] of fNames.entries()) {
 			console.log(`${idx + 1} : ${one}`);
 		}
 	}
 	saveBlob(iFileName: string, iBlob: string) {
 		//console.log(`dbg014: iFileName: ${iFileName}`);
-		this.fBlob.push({ fName: iFileName, fBlob: iBlob });
+		SysBlob.fNB.push({ fName: iFileName, fBlob: iBlob });
 	}
 	flushBlobs() {
-		this.fBlob = [];
+		SysBlob.fNB = [];
 	}
 	getBlobs(): tBlob[] {
-		return this.fBlob;
+		return SysBlob.fNB;
 	}
 	createZip(): string {
 		const rZip = 'this is a zip';
@@ -38,7 +44,11 @@ class sysBlob {
 	}
 }
 
-const sBlob = new sysBlob();
+function sysBlob(): SysBlob {
+	const rInst = new SysBlob();
+	return rInst;
+}
+const sBlob = sysBlob();
 
-export type { tBlob, sysBlob };
+export type { tBlob, SysBlob };
 export { sBlob };
