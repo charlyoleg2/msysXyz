7bac60b14508f1db97d64d97eed40f683f075682
charlyoleg <charles.braquet@gmx.fr>
2025-07-16 14:33:05 +0200

    enhance file handling

--- pkg/sysXyz-cli/src/systemcli.ts
+++ pkg/sysXyz-cli/src/systemcli.ts
@@ -7,6 +7,7 @@ import { sBlob } from 'systemix';
 
 async function writeOneTxtFile(iFileName: string, iFileBlob: string) {
 	try {
+		console.log(`Write file ${iFileName}`);
 		await fs.writeFile(iFileName, iFileBlob);
 	} catch (err) {
 		console.log(err);
@@ -15,8 +16,9 @@ async function writeOneTxtFile(iFileName: string, iFileBlob: string) {
 
 async function writeOutputFiles(outDirName: string) {
 	try {
+		await fs.rm(outDirName, { recursive: true, force: true }); // optional cleaning
 		await fs.mkdir(outDirName, { recursive: true });
-		console.log(`The directory ${outDirName} is created`);
+		console.log(`Create the directory ${outDirName}`);
 	} catch (err) {
 		console.error(err);
 	}
