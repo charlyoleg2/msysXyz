b6fc0b6059299c407a576080155756f14bc061de
charlyoleg <charles.braquet@gmx.fr>
2025-07-20 07:21:25 +0200

    post scripts

--- pkg/sysXyz-cli/scr/gen_stl_Xyz.sh
+++ pkg/sysXyz-cli/scr/gen_stl_Xyz.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 # gen_stl_Xyz.sh
 
+cd $(dirname $0)/..
+
 echo "gen_stl_Xyz.sh says Hello"
+npx desi77-cli --design desi77/square --param tmp/px_Xyz_stage1.json --outDir tmp2 --outFileName Xyz_stage1.scad write scad_3d_openscad
 echo "gen_stl_Xyz.sh says Bye"
