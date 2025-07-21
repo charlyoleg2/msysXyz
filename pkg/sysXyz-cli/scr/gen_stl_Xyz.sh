27afd455cd0f7039a4bfb2d81860043c09358eff
charlyoleg <charles.braquet@gmx.fr>
2025-07-21 04:08:27 +0200

    enhance openscad script

--- pkg/sysXyz-cli/scr/gen_stl_Xyz.sh
+++ pkg/sysXyz-cli/scr/gen_stl_Xyz.sh
@@ -23,5 +23,7 @@ echo "generate STL-assembly with OpenSCAD"
 cp scr/Xyz_assembly.scad tmp2/
 openscad -o tmp2/Xyz_assembly.stl tmp2/Xyz_assembly.scad
 
+echo "ls -ltra pkg/sysXyz-cli/tmp2"
 ls -ltra tmp2
+echo "(cd pkg/sysXyz-cli && f3d tmp2/Xyz_assembly.stl)"
 echo "gen_stl_Xyz.sh says Bye"
