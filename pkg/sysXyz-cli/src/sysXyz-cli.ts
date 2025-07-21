c0ec3af6818a7d011afc52be17b04e2f33d1a88d
charlyoleg <charles.braquet@gmx.fr>
2025-07-22 00:35:28 +0200

    using the generated script genStl

--- pkg/sysXyz-cli/src/sysXyz-cli.ts
+++ pkg/sysXyz-cli/src/sysXyz-cli.ts
@@ -13,12 +13,33 @@ import { sBlob } from 'sysXyz'; // workaround for the singleton issue
 // instanciate the single reference to SysBlob
 //const sBlob = sysBlob();
 
+const genStlOscadStart = `#!/usr/bin/env bash
+# gen_stl_Xyz.sh
+
+cd $(dirname $0)/..
+
+echo "gen_stl_Xyz.sh says Hello"
+`;
+const genStlOscadEnd = `
+echo "generate STL-assembly with OpenSCAD"
+cp scr/Xyz_assembly.scad tmp2/
+openscad -o tmp2/Xyz_assembly.stl tmp2/Xyz_assembly.scad
+
+echo "ls -ltra pkg/sysXyz-cli/tmp2"
+ls -ltra tmp2
+echo "(cd pkg/sysXyz-cli && f3d tmp2/Xyz_assembly.stl)"
+echo "gen_stl_Xyz.sh says Bye"
+`;
+
 // compute the system
 try {
 	// compute the component compXyz
 	const compXyzOut = compXyzSDef.compCompute(compXyzIn);
 	generateOutputFiles(compXyzIn.instName, compXyzOut, sBlob);
-	sBlob.saveBlob(`gen_stl_${compXyzIn.instName}.sh`, sBlob.getPartialBlob('genStlOscad'));
+	sBlob.saveBlob(
+		`gen_stl_${compXyzIn.instName}.sh`,
+		genStlOscadStart + sBlob.getPartialBlob('genStlOscad') + genStlOscadEnd
+	);
 	console.log(`[top-level err] ${compXyzOut.calcErr}`);
 	console.log(`[top-level log] ${compXyzOut.logstr}`);
 	// Use some results
