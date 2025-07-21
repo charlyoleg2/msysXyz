d587174d879453c27d12913b93c0aa02ba878017
charlyoleg <charles.braquet@gmx.fr>
2025-07-21 05:40:35 +0200

    freecad script

--- pkg/sysXyz-cli/scr/Xyz_assembly_fc.py
+++ pkg/sysXyz-cli/scr/Xyz_assembly_fc.py
@@ -3,7 +3,7 @@
 # freecad.cmd myScript.py
 
 import FreeCAD as App
-import Part
+#import Part
 import Mesh
 
 #print(sys.argv)
@@ -12,21 +12,41 @@ if (len(sys.argv) == 3):
     outFileName = sys.argv[2]
 print(f"outFileName: {outFileName}")
 
-def fex_Xyz_assembly():
-	MEX = Mesh.Mesh('Xyz_stage1.stl')
-	shape = Part.Shape()
-	shape.makeShapeFromMesh(MEX.Topology, 0.05)
-	solid = Part.makeSolid(shape)
-	VEX = solid
-	VR1 = VEX.rotate(App.Vector(0, 0, 0), App.Vector(1, 0, 0), 0.0000)
-	VR2 = VR1.rotate(App.Vector(0, 0, 0), App.Vector(0, 1, 0), 0.0000)
-	VR3 = VR2.rotate(App.Vector(0, 0, 0), App.Vector(0, 0, 1), 0.0000)
-	VFP = VR3.translate(App.Vector(0.0000, 0.0000, 0.0000))
-	return VFP
-Xyz_assembly = fex_Xyz_assembly()
+### converting Mesh to Part
+#def fex_Xyz_assembly():
+#	MEX = Mesh.Mesh('Xyz_stage1.stl')
+#	shape = Part.Shape()
+#	shape.makeShapeFromMesh(MEX.Topology, 0.05)
+#	solid = Part.makeSolid(shape)
+#	VEX = solid
+#	VR1 = VEX.rotate(App.Vector(0, 0, 0), App.Vector(1, 0, 0), 0.0000)
+#	VR2 = VR1.rotate(App.Vector(0, 0, 0), App.Vector(0, 1, 0), 0.0000)
+#	VR3 = VR2.rotate(App.Vector(0, 0, 0), App.Vector(0, 0, 1), 0.0000)
+#	VFP = VR3.translate(App.Vector(0.0000, 0.0000, 0.0000))
+#	return VFP
+#Xyz_assembly = fex_Xyz_assembly()
+#
+#Xyz_assembly.check()
+##Xyz_assembly.exportBrep(f"{outFileName}.brep")
+##Xyz_assembly.exportIges(f"{outFileName}.igs")
+##Xyz_assembly.exportStep(f"{outFileName}.stp")
+#Xyz_assembly.exportStl(f"{outFileName}.stl")
 
-Xyz_assembly.check()
-#Xyz_assembly.exportBrep(f"{outFileName}.brep")
-#Xyz_assembly.exportIges(f"{outFileName}.igs")
-#Xyz_assembly.exportStep(f"{outFileName}.stp")
-Xyz_assembly.exportStl(f"{outFileName}.stl")
+### directly working with Mesh
+def mesh_Xyz_assembly():
+	STAGE1 = Mesh.Mesh('Xyz_stage1.stl')
+	STAGE1.Placement.Base = (App.Vector(0, 0, 0))
+	STAGE2 = Mesh.Mesh('Xyz_stage2.stl')
+	STAGE2.Placement.Base = (App.Vector(0, 0, 100))
+	STAGE3 = Mesh.Mesh('Xyz_stage3.stl')
+	STAGE3.Placement.Base = (App.Vector(0, 0, 200))
+	STAGE2_Refine = Mesh.Mesh('Xyz_stage2_refine.stl')
+	STAGE2_Refine.Placement.Base = (App.Vector(0, 0, 100))
+	assembly = STAGE1
+	#assembly = assembly.unite(STAGE2)
+	assembly = assembly.unite(STAGE3)
+	assembly = assembly.unite(STAGE2_Refine)
+	#assembly.write(f"{outFileName}.stl", "AST")
+	assembly.write(f"{outFileName}.stl")
+
+mesh_Xyz_assembly()
